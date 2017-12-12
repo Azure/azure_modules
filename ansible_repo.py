7889acedc9658fc9cc0d572cfb85f34b24f75fc3
Zhijun Zhao <zhijzhao@Zhijuns-MacBook-Pro.local>
2017-12-12 16:42:41 +0800

    new progress

--- ansible_repo.py
+++ ansible_repo.py
@@ -1,8 +1,14 @@
 from github import Github
 import json
 import io
+import os
+import shutil
 from time import gmtime, strftime
+from git import Repo
 
+ansible_repo_path = "/tmp/ansible_repo"
+azure_repo_path = "/tmp/azure_repo"
+log_file_path = "./commit_history.json"
 
 def get_latest_commit_sha(repo, branch, path):
     commits = repo.get_commits(sha=branch, path=path)
@@ -22,6 +28,13 @@ def has_new_content(repo, branch_name, log_file):
     return False
 
 
+def ansible_repo_has_new_content():
+    g = Github("2c09286ec369be4a550b558e8dfb94a7df1fbf8d")
+    ansible_org = g.get_organization("ansible")
+    remote_ansible_repo = ansible_org.get_repo("ansible")
+    return has_new_content(remote_ansible_repo, "devel", log_file_path)
+
+
 def create_branch(repo):
     role_master_sha = repo.get_branch("master").commit.sha
     new_branch_name = "refs/heads/integration-" + strftime("%Y-%m-%d-%H-%M-%S", gmtime())
@@ -35,15 +48,68 @@ def save_back_to_json(data, file_name):
         f.write(json.dumps(data, sort_keys=True, indent=2, ensure_ascii=False))
 
 
-# g = Github("2c09286ec369be4a550b558e8dfb94a7df1fbf8d")
-# ansible_org = g.get_organization("ansible")
-# ansible_repo = ansible_org.get_repo("ansible")
+def create_clean_dir(dir):
+    if os.path.exists(dir):
+        shutil.rmtree(dir)
+    os.makedirs(dir)
+
+
+def clone_repos():
+    print "Cloning repos to local..."
+    create_clean_dir(ansible_repo_path)
+    create_clean_dir(azure_repo_path)
+
+    azure_repo = Repo()
+    azure_repo.clone_from("https://github.com/Azure/azure_modules.git", azure_repo_path)
+
+    ansible_repo = Repo()
+    ansible_repo.clone_from("https://github.com/ansible/ansible.git", ansible_repo_path)
+
+
+def copy(path):
+    print "Copying " + path
+
+
+def copy_file():
+    print "Copying file..."
+
+
+def copy_folder():
+    print "Copying folder..."
+
+
 # azure_org = g.get_organization("Azure")
 # role_repo = azure_org.get_repo("azure_modules")
 
+def migrate_contents():
+    g = Github("2c09286ec369be4a550b558e8dfb94a7df1fbf8d")
+    ansible_org = g.get_organization("ansible")
+    remote_ansible_repo = ansible_org.get_repo("ansible")
+    commit_history = json.load(open(log_file_path))
+
+    changed = False
+    for path, sha in commit_history.iteritems():
+        new_sha = get_latest_commit_sha(remote_ansible_repo, "devel", path)
+        if sha != new_sha:
+            copy(path)
+            changed = True
+            commit_history[path]=new_sha
+
+    if changed:
+        save_back_to_json(commit_history, log_file_path)
+
+
 def main():
-    print "hello"
+    print "Starting"
+
+    if not ansible_repo_has_new_content():
+        print "No new content. Exiting..."
+        return
+    else:
+        print "Migrating content from Ansible repo to Azure modules repo."
+
+    clone_repos()
 
 
 if __name__ == "__main__":
-    main()
+    migrate_contents()
