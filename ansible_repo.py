d923448dcd03dd0e81aba1a39ace23bd46e0162b
Zhijun Zhao <zhijzhao@Zhijuns-MacBook-Pro.local>
2017-12-13 11:33:24 +0800

    cleanup

--- ansible_repo.py
+++ ansible_repo.py
@@ -7,13 +7,14 @@ from time import gmtime, strftime
 from git import Repo
 from git import Actor
 
-github_access_token = "zhijun1988zhao8756c30e21c92e1e2eafba90b41bca8ced6a"
+github_access_token = ""
 ansible_repo_path = "/tmp/ansible_repo"
 azure_repo_path = "/tmp/azure_repo"
-log_file_path = "./commit_history.json"
-path_mapping_file_path = "./folder_mapping.json"
+sha_file_path = "/tmp/azure_repo/files/sha.json"
+repo_mapping_file_path = "./repo_mapping.json"
 local_azure_repo = None
 new_branch_name = None
+
 
 def get_latest_commit_sha(repo, branch, path):
     commits = repo.get_commits(sha=branch, path=path)
@@ -24,7 +25,16 @@ def get_latest_commit_sha(repo, branch, path):
         return None
 
 
+def clone_azure_repo():
+    print "Clone Azure repo to " + azure_repo_path
+    create_clean_dir(azure_repo_path)
+    global local_azure_repo
+    azure_repo = Repo()
+    local_azure_repo = azure_repo.clone_from("https://ZhijunZhao:" + github_access_token + "@github.com/Azure/azure_modules.git", azure_repo_path)
+
+
 def has_new_content(repo, branch_name, log_file):
+    clone_azure_repo()
     commit_history = json.load(open(log_file))
     for path, sha in commit_history.iteritems():
         new_sha = get_latest_commit_sha(repo, branch_name, path)
@@ -37,7 +47,7 @@ def ansible_repo_has_new_content():
     g = Github(github_access_token)
     ansible_org = g.get_organization("ansible")
     remote_ansible_repo = ansible_org.get_repo("ansible")
-    return has_new_content(remote_ansible_repo, "devel", log_file_path)
+    return has_new_content(remote_ansible_repo, "devel", sha_file_path)
 
 
 def create_branch(repo):
@@ -59,17 +69,11 @@ def create_clean_dir(dir):
     os.makedirs(dir)
 
 
-def clone_repos():
-    print "Cloning repos to local..."
-    #create_clean_dir(ansible_repo_path)
-    #create_clean_dir(azure_repo_path)
-
-    global local_azure_repo
-    azure_repo = Repo()
-    local_azure_repo = azure_repo.clone_from("https://ZhijunZhao:" + github_access_token + "@github.com/Azure/azure_modules.git", azure_repo_path)
-
-    #ansible_repo = Repo()
-    #ansible_repo.clone_from("https://github.com/ansible/ansible.git", ansible_repo_path)
+def clone_ansible_repo():
+    print "Clone Ansible repo to " + ansible_repo_path
+    create_clean_dir(ansible_repo_path)
+    ansible_repo = Repo()
+    ansible_repo.clone_from("https://github.com/ansible/ansible.git", ansible_repo_path)
 
 
 def copy(path):
@@ -80,11 +84,11 @@ def copy(path):
         copy_file(path)
 
 
-def get_joined_path(path):
-    path_map = json.load(open(path_mapping_file_path))
-    azure_path = path_map[path]
+def get_joined_path(ansible_path):
+    repo_map = json.load(open(repo_mapping_file_path))
+    azure_path = repo_map[ansible_path]
 
-    src = os.path.join(ansible_repo_path, path)
+    src = os.path.join(ansible_repo_path, ansible_path)
     dest = os.path.join(azure_repo_path, azure_path)
 
     return src, dest
@@ -126,12 +130,13 @@ def check_out_new_branch():
     print "Creating new branch " + new_branch_name
     local_azure_repo.git.checkout('HEAD', b=new_branch_name)
 
+
 def copy_changed_files():
     print "Copying changes..."
     g = Github(github_access_token)
     ansible_org = g.get_organization("ansible")
     remote_ansible_repo = ansible_org.get_repo("ansible")
-    commit_history = json.load(open(log_file_path))
+    commit_history = json.load(open(sha_file_path))
 
     changed = False
     for path, sha in commit_history.iteritems():
@@ -142,7 +147,7 @@ def copy_changed_files():
             commit_history[path]=new_sha
 
     if changed:
-        save_back_to_json(commit_history, log_file_path)
+        save_back_to_json(commit_history, sha_file_path)
 
 
 def push_changes_to_remote():
@@ -166,6 +171,7 @@ def send_pull_request():
 
 
 def migrate_contents():
+    clone_ansible_repo()
     check_out_new_branch()
     copy_changed_files()
     push_changes_to_remote()
@@ -181,7 +187,6 @@ def main():
     else:
         print "Migrating content from Ansible repo to Azure modules repo..."
 
-    clone_repos()
     migrate_contents()
 
 
