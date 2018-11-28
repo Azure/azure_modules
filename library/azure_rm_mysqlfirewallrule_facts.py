a2584f53c82fc4ae117f8b29eb447fe9fb21c9b9
Microsoft Visual Studio China Ansible on Azure robot <ansibleazurebot@microsoft.com>
2018-11-28 08:53:44 +0000

    Merged changes of Azure modules from Ansible repo

--- library/azure_rm_mysqlfirewallrule_facts.py
+++ library/azure_rm_mysqlfirewallrule_facts.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright (c) 2017 Zim Kalinowski, <zikalino@microsoft.com>
+# Copyright (c) 2018 Zim Kalinowski, <zikalino@microsoft.com>
 #
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
@@ -15,16 +15,16 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 
 DOCUMENTATION = '''
 ---
-module: azure_rm_postgresqldatabase_facts
-version_added: "2.7"
-short_description: Get Azure PostgreSQL Database facts.
+module: azure_rm_mysqlfirewallrule_facts
+version_added: "2.8"
+short_description: Get Azure MySQL Firewall Rule facts.
 description:
-    - Get facts of PostgreSQL Database.
+    - Get facts of Azure MySQL Firewall Rule.
 
 options:
     resource_group:
         description:
-            - The name of the resource group that contains the resource. You can obtain this value from the Azure Resource Manager API or the portal.
+            - The name of the resource group.
         required: True
     server_name:
         description:
@@ -32,7 +32,7 @@ options:
         required: True
     name:
         description:
-            - The name of the database.
+            - The name of the server firewall rule.
 
 extends_documentation_fragment:
     - azure
@@ -43,21 +43,21 @@ author:
 '''
 
 EXAMPLES = '''
-  - name: Get instance of PostgreSQL Database
-    azure_rm_postgresqldatabase_facts:
+  - name: Get instance of MySQL Firewall Rule
+    azure_rm_mysqlfirewallrule_facts:
       resource_group: resource_group_name
       server_name: server_name
-      name: database_name
+      name: firewall_rule_name
 
-  - name: List instances of PostgreSQL Database
-    azure_rm_postgresqldatabase_facts:
+  - name: List instances of MySQL Firewall Rule
+    azure_rm_mysqlfirewallrule_facts:
       resource_group: resource_group_name
       server_name: server_name
 '''
 
 RETURN = '''
-databases:
-    description: A list of dict results where the key is the name of the PostgreSQL Database and the values are the facts for that PostgreSQL Database.
+rules:
+    description: A list of dictionaries containing facts for MySQL Firewall Rule.
     returned: always
     type: complex
     contains:
@@ -66,17 +66,11 @@ databases:
                 - Resource ID
             returned: always
             type: str
-            sample: "/subscriptions/ffffffff-ffff-ffff-ffff-ffffffffffff/resourceGroups/TestGroup/providers/Microsoft.DBforPostgreSQL/servers/testser
-                    ver/databases/db1"
-        resource_group:
-            description:
-                - Resource group name.
-            returned: always
-            type: str
-            sample: testrg
+            sample: "/subscriptions/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/TestGroup/providers/Microsoft.DBforMySQL/servers/testserver/fire
+                    wallRules/rule1"
         server_name:
             description:
-                - Server name.
+                - The name of the server.
             returned: always
             type: str
             sample: testserver
@@ -85,33 +79,34 @@ databases:
                 - Resource name.
             returned: always
             type: str
-            sample: db1
-        charset:
+            sample: rule1
+        start_ip_address:
             description:
-                - The charset of the database.
+                - The start IP address of the MySQL firewall rule.
             returned: always
             type: str
-            sample: UTF8
-        collation:
+            sample: 10.0.0.16
+        end_ip_address:
             description:
-                - The collation of the database.
+                - The end IP address of the MySQL firewall rule.
             returned: always
             type: str
-            sample: English_United States.1252
+            sample: 10.0.0.18
 '''
 
 from ansible.module_utils.azure_rm_common import AzureRMModuleBase
 
 try:
     from msrestazure.azure_exceptions import CloudError
-    from azure.mgmt.rdbms.postgresql import PostgreSQLManagementClient
+    from msrestazure.azure_operation import AzureOperationPoller
+    from azure.mgmt.rdbms.mysql import MySQLManagementClient
     from msrest.serialization import Model
 except ImportError:
     # This is handled in azure_rm_common
     pass
 
 
-class AzureRMDatabasesFacts(AzureRMModuleBase):
+class AzureRMMySQLFirewallRulesFacts(AzureRMModuleBase):
     def __init__(self):
         # define user inputs into argument
         self.module_arg_spec = dict(
@@ -131,34 +126,34 @@ class AzureRMDatabasesFacts(AzureRMModuleBase):
         self.results = dict(
             changed=False
         )
+        self.mgmt_client = None
         self.resource_group = None
         self.server_name = None
         self.name = None
-        super(AzureRMDatabasesFacts, self).__init__(self.module_arg_spec, supports_tags=False)
+        super(AzureRMMySQLFirewallRulesFacts, self).__init__(self.module_arg_spec, supports_tags=False)
 
     def exec_module(self, **kwargs):
         for key in self.module_arg_spec:
             setattr(self, key, kwargs[key])
+        self.mgmt_client = self.get_mgmt_svc_client(MySQLManagementClient,
+                                                    base_url=self._cloud_environment.endpoints.resource_manager)
 
-        if (self.resource_group is not None and
-                self.server_name is not None and
-                self.name is not None):
-            self.results['databases'] = self.get()
-        elif (self.resource_group is not None and
-              self.server_name is not None):
-            self.results['databases'] = self.list_by_server()
+        if (self.name is not None):
+            self.results['rules'] = self.get()
+        else:
+            self.results['rules'] = self.list_by_server()
         return self.results
 
     def get(self):
         response = None
         results = []
         try:
-            response = self.postgresql_client.databases.get(resource_group_name=self.resource_group,
-                                                            server_name=self.server_name,
-                                                            database_name=self.name)
+            response = self.mgmt_client.firewall_rules.get(resource_group_name=self.resource_group,
+                                                           server_name=self.server_name,
+                                                           firewall_rule_name=self.name)
             self.log("Response : {0}".format(response))
         except CloudError as e:
-            self.log('Could not get facts for Databases.')
+            self.log('Could not get facts for FirewallRules.')
 
         if response is not None:
             results.append(self.format_item(response))
@@ -169,11 +164,11 @@ class AzureRMDatabasesFacts(AzureRMModuleBase):
         response = None
         results = []
         try:
-            response = self.postgresql_client.databases.list_by_server(resource_group_name=self.resource_group,
-                                                                       server_name=self.server_name)
+            response = self.mgmt_client.firewall_rules.list_by_server(resource_group_name=self.resource_group,
+                                                                      server_name=self.server_name)
             self.log("Response : {0}".format(response))
         except CloudError as e:
-            self.fail("Error listing for server {0} - {1}".format(self.server_name, str(e)))
+            self.log('Could not get facts for FirewallRules.')
 
         if response is not None:
             for item in response:
@@ -185,16 +180,17 @@ class AzureRMDatabasesFacts(AzureRMModuleBase):
         d = item.as_dict()
         d = {
             'resource_group': self.resource_group,
+            'id': d['id'],
             'server_name': self.server_name,
             'name': d['name'],
-            'charset': d['charset'],
-            'collation': d['collation']
+            'start_ip_address': d['start_ip_address'],
+            'end_ip_address': d['end_ip_address']
         }
         return d
 
 
 def main():
-    AzureRMDatabasesFacts()
+    AzureRMMySQLFirewallRulesFacts()
 
 
 if __name__ == '__main__':
