7c2fc957f36bc339153ac3939018a346c0fc369c
Microsoft Visual Studio China Ansible on Azure robot <37099836+ansibleazurebot@users.noreply.github.com>
2018-12-20 14:20:18 +0800

    Merged changes of Azure modules from Ansible repo (#78)

--- library/azure_rm_rediscache.py
+++ library/azure_rm_rediscache.py
@@ -112,7 +112,7 @@ options:
             - Dict of tenant settings.
     state:
       description:
-        - Assert the state of the redis cahce.
+        - Assert the state of the redis cache.
         - Use 'present' to create or update a redis cache and 'absent' to delete it.
       default: present
       choices:
@@ -163,14 +163,14 @@ RETURN = '''
 id:
     description: Id of the redis cache.
     returned: always
-    type: string
+    type: str
     sample: {
         "id": "/subscriptions/<subs_id>/resourceGroups/rg/providers/Microsoft.Cache/Redis/redis1"
     }
 host_name:
     description: Host name of the redis cache.
     returned: state is present
-    type: string
+    type: str
     sample: {
         "host_name": "redis1.redis.cache.windows.net"
     }
@@ -285,7 +285,7 @@ class AzureRMRedisCaches(AzureRMModuleBase):
                 type='int'
             ),
             shard_count=dict(
-                type='ints'
+                type='int'
             ),
             static_ip=dict(
                 type='str'
